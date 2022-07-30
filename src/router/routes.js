87ec5c6c89ec136b65182c77000d7301de6e2d93
Jhozua Diaz <diaz.jhozua@gmail.com>
2022-07-30 17:00:31 +0800

    Added empty components for login and register

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,10 +9,16 @@ export default [
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: () => import("../views/AboutView.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/Auth/LoginView.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("../views/Auth/RegisterView.vue"),
   },
 ];
