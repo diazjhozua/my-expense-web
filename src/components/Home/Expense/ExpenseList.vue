a7857d9b899931821ab5a817390602634b5cdc21
Jhozua Diaz <diaz.jhozua@gmail.com>
2022-08-03 12:23:02 +0800

    Created Bottom Navigation

---
+++ src/components/Home/Expense/ExpenseList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Expenses</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,51 @@
 <template>
-  <div>hello</div>
+  <section class="overflow-hidden mx-auto" height="100%">
+    <v-bottom-navigation
+      v-model="bottomNavigation"
+      absolute
+      horizontal
+      color="grey darken-4"
+    >
+      <v-spacer></v-spacer>
+      <v-btn color="teal" text>
+        <span>Expenses</span>
+
+        <v-icon>mdi-cash</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn color="teal" text>
+        <span>Categories</span>
+
+        <v-icon>mdi-shape</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+    </v-bottom-navigation>
+
+    <v-responsive class="overflow-y-auto" max-height="600">
+      <v-responsive height="1500">
+        <expense-list v-if="bottomNavigation == 0"></expense-list>
+        <category-list v-if="bottomNavigation == 1"></category-list>
+      </v-responsive>
+    </v-responsive>
+  </section>
 </template>
 
 <script>
-// import HelloWorld from "../components/HelloWorld";
-
+import ExpenseList from "@/components/Home/Expense/ExpenseList.vue";
+import CategoryList from "@/components/Home/Category/CategoryList.vue";
 export default {
   name: "HomeView",
-
+  metaInfo: {
+    title: "Home",
+  },
   components: {
-    // HelloWorld,
+    ExpenseList,
+    CategoryList,
+  },
+  data() {
+    return {
+      bottomNavigation: 0,
+    };
   },
 };
 </script>
